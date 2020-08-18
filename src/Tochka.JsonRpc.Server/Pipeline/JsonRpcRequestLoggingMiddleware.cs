ba01b6f7ad24674878fecd278622d42e77215bb0
Rostislav Kirillov <rast@tochka.com>
2020-08-18 19:09:36 +0500

    feat(server): add sample request-response logging for reference

---
+++ src/Tochka.JsonRpc.Server/Pipeline/JsonRpcRequestLoggingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Tochka.JsonRpc.Common.Models.Request.Untyped;
+
+namespace Tochka.JsonRpc.Server.Pipeline
+{
+    public class JsonRpcRequestLoggingMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<JsonRpcRequestLoggingMiddleware> log;
+
+        public JsonRpcRequestLoggingMiddleware(RequestDelegate next, ILogger<JsonRpcRequestLoggingMiddleware> log)
+        {
+            this.next = next;
+            this.log = log;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            log.LogTrace($"Started");
+            var call = context.GetJsonRpcCall();
+            switch (call)
+            {
+                case UntypedNotification untypedNotification:
+                    log.LogInformation("JsonRpc notification [{method}]. Raw json: [{rawJson}]", untypedNotification.Method, untypedNotification.RawJson);
+                    break;
+                case UntypedRequest untypedRequest:
+                    log.LogInformation("JsonRpc request [{method}], id [{id}]. Raw json: [{rawJson}]", untypedRequest.Method, untypedRequest.Id, untypedRequest.RawJson);
+                    break;
+            }
+
+            await next(context);
+            log.LogTrace($"Completed");
+        }
+    }
+}
---
+++ src/Tochka.JsonRpc.Server/Pipeline/JsonRpcResultLoggingFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Tochka.JsonRpc.Common;
+using Tochka.JsonRpc.Common.Serializers;
+using Tochka.JsonRpc.Server.Models;
+
+namespace Tochka.JsonRpc.Server.Pipeline
+{
+    public class JsonRpcResultLoggingFilter : IActionFilter
+    {
+        private readonly ILogger log;
+
+        public JsonRpcResultLoggingFilter(ILogger<JsonRpcResultLoggingFilter> log)
+        {
+            this.log = log;
+        }
+
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            return;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            log.LogTrace($"{nameof(OnActionExecuted)} Started");
+            if (context.HttpContext.Items.TryGetValue(JsonRpcConstants.ActionResultTypeItemKey, out var resultType))
+            {
+                var methodMetadata = context.ActionDescriptor.GetProperty<MethodMetadata>() ?? throw new ArgumentNullException(nameof(MethodMetadata));
+                var serializer = context.HttpContext.RequestServices.GetRequiredService(methodMetadata.MethodOptions.RequestSerializer) as IJsonRpcSerializer;
+                switch (context.Result)
+                {
+                    case ObjectResult objectResult:
+                        var jsonValue = JsonConvert.SerializeObject(objectResult.Value, serializer.Settings);
+                        log.LogInformation("JsonRpc Action ObjectResult {code}: {value}", objectResult.StatusCode, jsonValue);
+                        break;
+                    default:
+                        log.LogInformation("JsonRpc Action result [{type}]: {result}", resultType, context.Result);
+                        break;
+                }
+            }
+            else
+            {
+                log.LogTrace($"Not logging action result because type was not provided by {nameof(JsonRpcFilter)}");
+            }
+
+            log.LogTrace($"{nameof(OnActionExecuting)} Completed");
+        }
+    }
+}
