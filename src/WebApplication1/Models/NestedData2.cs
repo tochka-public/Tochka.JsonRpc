7111a32af2731b648cb1ce7eed153eb3f0997f5f
Rostislav Kirillov <superrast@gmail.com>
2020-12-10 17:47:37 +0500

    fix: add project descriptions, remove trash

--- src/WebApplication1/Models/NestedData2.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.Serialization;
-
-namespace WebApplication1.Controllers
-{
-    [DataContract(Name = "overriden-nested-name")]
-    public class NestedData2 : NestedData
-    {
-    }
-}
--- src/WebApplication1/Models/TestData.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication1.Controllers
-{
-    public class TestData
-    {
-        public int Value { get; set; }
-        public string AnotherValue { get; set; }
-    }
-}
