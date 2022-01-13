c09e9e4eb6127056383648082c100cf032dfc541
Дмитрий Белокобыльский <belo.dima@gmail.com>
2022-01-13 12:40:01 +0500

    add test project on net6.0 with netstandard2.0 jsonrpcServer

---
+++ src/JsonRpcServer/CorsStartupFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace JsonRpcServer;
+
+public class CorsStartupFilter : IStartupFilter
+{
+    public const string CorsPolicyName = "AllowAll";
+
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) =>
+        builder =>
+        {
+            _ = builder.UseCors(CorsPolicyName);
+            next(builder);
+        };
+}
---
+++ src/JsonRpcServer/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Tochka.JsonRpc.Server.Pipeline;
+
+namespace JsonRpcServer;
+
+public class TestController : JsonRpcController
+{
+    public Task<string> ToLower(string? text)
+    {
+        return Task.FromResult(text!.ToLower());
+    }
+}
