fa2825d2dc716339c0afc6dad15986ac7c3accff
Дмитрий Белокобыльский <belo.dima@gmail.com>
2022-02-14 17:13:25 +0500

    fix

--- src/tests/Tochka.JsonRpc.Server.IntegrationTests/TestAppFactory.cs
+++ src/tests/Tochka.JsonRpc.Server.IntegrationTests/TestAppFactory.cs
@@ -27,7 +27,7 @@ namespace Tochka.JsonRpc.Server.IntegrationTests
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseContentRoot(".");
-            // base.ConfigureWebHost(builder);
+            base.ConfigureWebHost(builder);
         }
     }
 }
