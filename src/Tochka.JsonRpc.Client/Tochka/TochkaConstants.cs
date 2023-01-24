9ab95cba4c151db8513ddaa781314dbcb6d18116
ekonyukov <eugene_ken@tochka.com>
2023-01-24 14:19:03 +0500

    remove TochkaJsonRpcClientBase

--- src/Tochka.JsonRpc.Client/Tochka/TochkaConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Tochka.JsonRpc.Client.Tochka
-{
-    public static class TochkaConstants
-    {
-        public const string DefaultAuthenticationHeader = "X-Tochka-Access-Key";
-    }
-}
--- src/tests/Tochka.JsonRpc.Client.Tests/TochkaJsonRpcClientBase/TochkaTestClientOptions.cs
+++
@@ -1,7 +0,0 @@
-using Tochka.JsonRpc.Client.Tochka;
-
-namespace Tochka.JsonRpc.Client.Tests.TochkaJsonRpcClientBase;
-
-public class TochkaTestClientOptions : TochkaJsonRpcClientOptionsBase
-{
-}
