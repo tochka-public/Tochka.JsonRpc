c998cdf8eb9059cdb4347c7eaa4cd9808e052da1
Artemiy Izakov <izakov@tochka.com>
2023-06-29 18:59:51 +0500

    test(server): add binding tests

---
+++ src/tests/Tochka.JsonRpc.Server.Tests/Binding/JsonRpcModelBinderTests.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using Tochka.JsonRpc.Common.Models.Request.Untyped;
+using Tochka.JsonRpc.Server.Attributes;
+using Tochka.JsonRpc.Server.Binding;
+using Tochka.JsonRpc.Server.Binding.ParseResults;
+using Tochka.JsonRpc.Server.Exceptions;
+using Tochka.JsonRpc.Server.Features;
+using Tochka.JsonRpc.Server.Metadata;
+using Tochka.JsonRpc.Server.Serialization;
+using Tochka.JsonRpc.Server.Settings;
+
+namespace Tochka.JsonRpc.Server.Tests.Binding;
+
+[TestFixture]
+internal class JsonRpcModelBinderTests
+{
+    private Mock<IJsonRpcParamsParser> paramsParserMock;
+    private Mock<IJsonRpcParameterBinder> parameterBinderMock;
+    private List<IJsonSerializerOptionsProvider> serializerOptionsProviders;
+    private JsonRpcServerOptions options;
+    private Mock<JsonRpcModelBinder> modelBinderMock;
+
+    [SetUp]
+    public void Setup()
+    {
+        paramsParserMock = new Mock<IJsonRpcParamsParser>();
+        parameterBinderMock = new Mock<IJsonRpcParameterBinder>();
+        serializerOptionsProviders = new List<IJsonSerializerOptionsProvider>();
+        options = new JsonRpcServerOptions();
+        modelBinderMock = new Mock<JsonRpcModelBinder>(paramsParserMock.Object, parameterBinderMock.Object, serializerOptionsProviders, Options.Create(options))
+        {
+            CallBase = true
+        };
+    }
+
+    [Test]
+    public async Task BindModelAsync_NoParametersMetadata_Throw()
+    {
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    EndpointMetadata = new List<object>()
+                }
+            }
+        };
+
+        var action = () => modelBinderMock.Object.BindModelAsync(bindingContext);
+
+        await action.Should().ThrowAsync<JsonRpcServerException>();
+    }
+
+    [Test]
+    public async Task BindModelAsync_NoParameterInMetadata_Throw()
+    {
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    EndpointMetadata = new List<object>
+                    {
+                        new JsonRpcActionParametersMetadata()
+                    }
+                }
+            },
+            FieldName = FieldName
+        };
+
+        var action = () => modelBinderMock.Object.BindModelAsync(bindingContext);
+
+        await action.Should().ThrowAsync<JsonRpcServerException>();
+    }
+
+    [Test]
+    public async Task BindModelAsync_HasParameterMetadata_ParseAndSetResult()
+    {
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    EndpointMetadata = new List<object>
+                    {
+                        new JsonRpcActionParametersMetadata
+                        {
+                            Parameters =
+                            {
+                                [FieldName] = parameterMetadata
+                            }
+                        }
+                    }
+                }
+            },
+            FieldName = FieldName
+        };
+        var parseResult = Mock.Of<IParseResult>();
+        modelBinderMock.Setup(b => b.Parse(bindingContext, parameterMetadata))
+            .ReturnsAsync(parseResult)
+            .Verifiable();
+        modelBinderMock.Setup(b => b.SetResult(parseResult, bindingContext, parameterMetadata))
+            .Returns(Task.CompletedTask)
+            .Verifiable();
+
+        await modelBinderMock.Object.BindModelAsync(bindingContext);
+
+        modelBinderMock.Verify();
+    }
+
+    [Test]
+    public async Task Parse_RawCallIsNull_Throw()
+    {
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        bindingContext.HttpContext.Features.Set<IJsonRpcFeature>(new JsonRpcFeature
+        {
+            Call = new UntypedNotification("method", null)
+        });
+
+        var action = () => modelBinderMock.Object.Parse(bindingContext, parameterMetadata);
+
+        await action.Should().ThrowAsync<JsonRpcServerException>();
+    }
+
+    [Test]
+    public async Task Parse_CallIsNull_Throw()
+    {
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        bindingContext.HttpContext.Features.Set<IJsonRpcFeature>(new JsonRpcFeature
+        {
+            RawCall = JsonDocument.Parse("{}")
+        });
+
+        var action = () => modelBinderMock.Object.Parse(bindingContext, parameterMetadata);
+
+        await action.Should().ThrowAsync<JsonRpcServerException>();
+    }
+
+    [Test]
+    public async Task Parse_BothCallsPresent_UseParser()
+    {
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        var rawCall = JsonDocument.Parse("{}");
+        var parameters = JsonDocument.Parse("{}");
+        bindingContext.HttpContext.Features.Set<IJsonRpcFeature>(new JsonRpcFeature
+        {
+            RawCall = rawCall,
+            Call = new UntypedNotification("method", parameters)
+        });
+        var parseResult = Mock.Of<IParseResult>();
+        paramsParserMock.Setup(p => p.Parse(rawCall, parameters, parameterMetadata))
+            .Returns(parseResult)
+            .Verifiable();
+
+        var result = await modelBinderMock.Object.Parse(bindingContext, parameterMetadata);
+
+        result.Should().Be(parseResult);
+        paramsParserMock.Verify();
+    }
+
+    [Test]
+    public async Task SetResult_NoCustomSerializer_UseDefaultDataJsonSerializerOptions()
+    {
+        var parseResult = Mock.Of<IParseResult>();
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    EndpointMetadata = new List<object>()
+                }
+            }
+        };
+        parameterBinderMock.Setup(b => b.SetResult(bindingContext, parameterMetadata, parseResult, options.DefaultDataJsonSerializerOptions))
+            .Verifiable();
+
+        await modelBinderMock.Object.SetResult(parseResult, bindingContext, parameterMetadata);
+
+        paramsParserMock.Verify();
+    }
+
+    [Test]
+    public async Task SetResult_CustomSerializerNotRegistered_Throw()
+    {
+        var parseResult = Mock.Of<IParseResult>();
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    EndpointMetadata = new List<object>
+                    {
+                        new JsonRpcSerializerOptionsAttribute(typeof(SnakeCaseJsonSerializerOptionsProvider))
+                    }
+                }
+            }
+        };
+
+        var action = () => modelBinderMock.Object.SetResult(parseResult, bindingContext, parameterMetadata);
+
+        await action.Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Test]
+    public async Task SetResult_CustomSerializer_UseCustomSerializerOptions()
+    {
+        var parseResult = Mock.Of<IParseResult>();
+        var parameterMetadata = new JsonRpcParameterMetadata("propertyName", 0, BindingStyle.Default, false, "originalName", typeof(string));
+        var bindingContext = new DefaultModelBindingContext
+        {
+            ActionContext = new ActionContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    EndpointMetadata = new List<object>
+                    {
+                        new JsonRpcSerializerOptionsAttribute(typeof(SnakeCaseJsonSerializerOptionsProvider))
+                    }
+                }
+            }
+        };
+        var serializerOptionsProvider = new SnakeCaseJsonSerializerOptionsProvider();
+        serializerOptionsProviders.Add(serializerOptionsProvider);
+        parameterBinderMock.Setup(b => b.SetResult(bindingContext, parameterMetadata, parseResult, serializerOptionsProvider.Options))
+            .Verifiable();
+
+        await modelBinderMock.Object.SetResult(parseResult, bindingContext, parameterMetadata);
+
+        parameterBinderMock.Verify();
+    }
+
+    private const string FieldName = "fieldName";
+}
---
+++ src/tests/Tochka.JsonRpc.Server.Tests/Binding/JsonRpcParameterModelConventionTests.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using Tochka.JsonRpc.Server.Attributes;
+using Tochka.JsonRpc.Server.Binding;
+using Tochka.JsonRpc.Server.Metadata;
+using Tochka.JsonRpc.Server.Serialization;
+using Tochka.JsonRpc.Server.Settings;
+
+namespace Tochka.JsonRpc.Server.Tests.Binding;
+
+[TestFixture]
+internal class JsonRpcParameterModelConventionTests
+{
+    private List<IJsonSerializerOptionsProvider> serializerOptionsProviders;
+    private JsonRpcServerOptions options;
+    private JsonRpcParameterModelConvention parameterModelConvention;
+
+    [SetUp]
+    public void Setup()
+    {
+        serializerOptionsProviders = new List<IJsonSerializerOptionsProvider>();
+        options = new JsonRpcServerOptions();
+
+        parameterModelConvention = new JsonRpcParameterModelConvention(serializerOptionsProviders, Options.Create(options));
+    }
+
+    [Test]
+    public void Apply_ActionNotFromJsonRpcController_DoNothing()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(Mock.Of<Type>());
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), new List<object>()),
+                Selectors = { selector }
+            }
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        parameterModel.BindingInfo.Should().BeNull();
+        selector.EndpointMetadata.Should().BeEmpty();
+    }
+
+    [Test]
+    public void Apply_BinderTypeNotJsonRpcModelBinder_DoNothing()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(Mock.Of<Type>());
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var bindingInfo = new BindingInfo
+        {
+            BinderType = Mock.Of<IModelBinder>().GetType()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            BindingInfo = bindingInfo
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        parameterModel.BindingInfo.Should().Be(bindingInfo);
+        selector.EndpointMetadata.Should().BeEmpty();
+    }
+
+    [Test]
+    public void Apply_NoBindingInfo_SetDefaultFromParamsBindingInfo()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(Mock.Of<Type>());
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            BindingInfo = null,
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        var expected = new BindingInfo
+        {
+            BinderType = typeof(JsonRpcModelBinder),
+            BindingSource = BindingSource.Custom
+        };
+        parameterModel.BindingInfo.Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public void Apply_NoParametersMetadata_AddMetadata()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(Mock.Of<Type>());
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        selector.EndpointMetadata.Should().Contain(static m => m is JsonRpcActionParametersMetadata);
+    }
+
+    [Test]
+    public void Apply_NoFromParamsAttribute_UseDefaultBindingStyle()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        var position = 123;
+        var type = Mock.Of<Type>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(type);
+        parameterInfo.Setup(static i => i.Position)
+            .Returns(position);
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        var expected = new JsonRpcActionParametersMetadata
+        {
+            Parameters =
+            {
+                [ParameterName] = new JsonRpcParameterMetadata("parameter_name", position, BindingStyle.Default, false, ParameterName, type)
+            }
+        };
+        selector.EndpointMetadata.Should().ContainEquivalentOf(expected);
+    }
+
+    [TestCase(BindingStyle.Default)]
+    [TestCase(BindingStyle.Object)]
+    [TestCase(BindingStyle.Array)]
+    public void Apply_HasFromParamsAttribute_UseBindingStyleFromAttribute(BindingStyle bindingStyle)
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        var position = 123;
+        var type = Mock.Of<Type>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(type);
+        parameterInfo.Setup(static i => i.Position)
+            .Returns(position);
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterAttributes = new List<object>
+        {
+            new FromParamsAttribute(bindingStyle)
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, parameterAttributes)
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        var expected = new JsonRpcActionParametersMetadata
+        {
+            Parameters =
+            {
+                [ParameterName] = new JsonRpcParameterMetadata("parameter_name", position, bindingStyle, false, ParameterName, type)
+            }
+        };
+        selector.EndpointMetadata.Should().ContainEquivalentOf(expected);
+    }
+
+    [Test]
+    public void Apply_ParameterIsOptional_MarkAsOptional()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        var position = 123;
+        var type = Mock.Of<Type>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(type);
+        parameterInfo.Setup(static i => i.Position)
+            .Returns(position);
+        parameterInfo.Setup(static i => i.Attributes)
+            .Returns(ParameterAttributes.Optional);
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        var expected = new JsonRpcActionParametersMetadata
+        {
+            Parameters =
+            {
+                [ParameterName] = new JsonRpcParameterMetadata("parameter_name", position, BindingStyle.Default, true, ParameterName, type)
+            }
+        };
+        selector.EndpointMetadata.Should().ContainEquivalentOf(expected);
+    }
+
+    [Test]
+    public void Apply_NoCustomSerializer_UseDefaultDataJsonSerializerOptions()
+    {
+        var selector = new SelectorModel();
+        var parameterInfo = new Mock<ParameterInfo>();
+        var position = 123;
+        var type = Mock.Of<Type>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(type);
+        parameterInfo.Setup(static i => i.Position)
+            .Returns(position);
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        var expected = new JsonRpcActionParametersMetadata
+        {
+            Parameters =
+            {
+                [ParameterName] = new JsonRpcParameterMetadata("parameter_name", position, BindingStyle.Default, false, ParameterName, type)
+            }
+        };
+        selector.EndpointMetadata.Should().ContainEquivalentOf(expected);
+    }
+
+    [Test]
+    public void Apply_CustomSerializerNotRegistered_Throw()
+    {
+        var selector = new SelectorModel
+        {
+            EndpointMetadata =
+            {
+                new JsonRpcSerializerOptionsAttribute(typeof(SnakeCaseJsonSerializerOptionsProvider))
+            }
+        };
+        var parameterInfo = new Mock<ParameterInfo>();
+        var position = 123;
+        var type = Mock.Of<Type>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(type);
+        parameterInfo.Setup(static i => i.Position)
+            .Returns(position);
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        var action = () => parameterModelConvention.Apply(parameterModel);
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Apply_CustomSerializer_UseDefaultDataJsonSerializerOptions()
+    {
+        var selector = new SelectorModel
+        {
+            EndpointMetadata =
+            {
+                new JsonRpcSerializerOptionsAttribute(typeof(CamelCaseJsonSerializerOptionsProvider))
+            }
+        };
+        var parameterInfo = new Mock<ParameterInfo>();
+        var position = 123;
+        var type = Mock.Of<Type>();
+        parameterInfo.Setup(static i => i.ParameterType)
+            .Returns(type);
+        parameterInfo.Setup(static i => i.Position)
+            .Returns(position);
+        var controllerAttributes = new List<object>
+        {
+            new JsonRpcControllerAttribute()
+        };
+        serializerOptionsProviders.Add(new CamelCaseJsonSerializerOptionsProvider());
+        var parameterModel = new ParameterModel(parameterInfo.Object, new List<object>())
+        {
+            Action = new ActionModel(Mock.Of<MethodInfo>(), new List<object>())
+            {
+                Controller = new ControllerModel(Mock.Of<TypeInfo>(), controllerAttributes),
+                Selectors = { selector }
+            },
+            ParameterName = ParameterName
+        };
+
+        parameterModelConvention.Apply(parameterModel);
+
+        var expected = new JsonRpcActionParametersMetadata
+        {
+            Parameters =
+            {
+                [ParameterName] = new JsonRpcParameterMetadata("parameterName", position, BindingStyle.Default, false, ParameterName, type)
+            }
+        };
+        selector.EndpointMetadata.Should().ContainEquivalentOf(expected);
+    }
+
+    private const string ParameterName = "parameterName";
+}
