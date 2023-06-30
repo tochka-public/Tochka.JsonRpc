934343a1753f6d51c0000d4280be416f48655ef4
Artemiy Izakov <izakov@tochka.com>
2023-06-30 12:49:37 +0500

    test(server): add filters tests

--- src/Tochka.JsonRpc.Server/Filters/JsonRpcActionFilter.cs
+++ src/Tochka.JsonRpc.Server/Filters/JsonRpcActionFilter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Tochka.JsonRpc.Server.Extensions;
 
 namespace Tochka.JsonRpc.Server.Filters;
 
@@ -9,6 +10,11 @@ internal class JsonRpcActionFilter : IActionFilter
     // if model binding failed
     public void OnActionExecuting(ActionExecutingContext context)
     {
+        if (context.HttpContext.GetJsonRpcCall() == null)
+        {
+            return;
+        }
+
         if (context.Result != null || context.ModelState.IsValid)
         {
             return;
