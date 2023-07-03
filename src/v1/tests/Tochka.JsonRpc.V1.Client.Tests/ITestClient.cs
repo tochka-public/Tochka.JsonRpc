8c7555a1d3b9ceb3d25ad56128542e43e9898892
Artemiy Izakov <izakov@tochka.com>
2023-07-03 18:10:38 +0500

    test(v2): remove rubbish from coverage

--- src/v1/tests/Tochka.JsonRpc.V1.Client.Tests/ITestClient.cs
+++
@@ -1,9 +0,0 @@
-using Tochka.JsonRpc.V1.Client;
-
-namespace Tochka.JsonRpc.V1.Client.Tests
-{
-    public interface ITestClient : IJsonRpcClient
-    {
-
-    }
-}
--- src/v1/tests/Tochka.JsonRpc.V1.Server.IntegrationTests/Models/TestData.cs
+++
@@ -1,8 +0,0 @@
-namespace Tochka.JsonRpc.V1.Server.IntegrationTests.Models
-{
-    public class TestData
-    {
-        public string Value { get; set; } = "test";
-        public int OtherValue { get; set; } = 1;
-    }
-}
