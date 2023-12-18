ac1e52480f3269ba3b2cebbc0d48901e791ed9fb
Artemiy Izakov <izakov@tochka.com>
2023-12-18 18:15:52 +0500

    feat(server): add exceptions logging

--- src/Tochka.JsonRpc.Server/Filters/JsonRpcExceptionFilter.cs
+++ src/Tochka.JsonRpc.Server/Filters/JsonRpcExceptionFilter.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Tochka.JsonRpc.Server.Extensions;
 using Tochka.JsonRpc.Server.Services;
+using Tochka.JsonRpc.Server.Settings;
 
 namespace Tochka.JsonRpc.Server.Filters;
 
 /// <inheritdoc />
 /// <summary>
-/// Filter for JSON-RPC actions to convert exceptions to JSON-RPC error
+/// Filter for JSON-RPC actions to log exceptions and convert them to JSON-RPC error
 /// </summary>
 internal class JsonRpcExceptionFilter : IExceptionFilter
 {
     private readonly IJsonRpcErrorFactory errorFactory;
+    private readonly JsonRpcServerOptions options;
+    private readonly ILogger<JsonRpcExceptionFilter> log;
 
-    public JsonRpcExceptionFilter(IJsonRpcErrorFactory errorFactory) => this.errorFactory = errorFactory;
+    public JsonRpcExceptionFilter(IJsonRpcErrorFactory errorFactory, IOptions<JsonRpcServerOptions> options, ILogger<JsonRpcExceptionFilter> log)
+    {
+        this.errorFactory = errorFactory;
+        this.options = options.Value;
+        this.log = log;
+    }
 
     // wrap exception in json rpc error
     public void OnException(ExceptionContext context)
@@ -21,6 +31,11 @@ internal class JsonRpcExceptionFilter : IExceptionFilter
         if (context.HttpContext.GetJsonRpcCall() == null)
         {
             return;
+        }
+
+        if (options.LogExceptions)
+        {
+            log.LogError(context.Exception, "Exception during JSON-RPC call processing");
         }
 
         var error = errorFactory.Exception(context.Exception);
--- src/tests/Tochka.JsonRpc.Server.Tests/Filters/JsonRpcExceptionFilterTests.cs
+++ src/tests/Tochka.JsonRpc.Server.Tests/Filters/JsonRpcExceptionFilterTests.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
 using Tochka.JsonRpc.Common.Models.Request;
@@ -14,6 +16,7 @@ using Tochka.JsonRpc.Common.Models.Response.Errors;
 using Tochka.JsonRpc.Server.Features;
 using Tochka.JsonRpc.Server.Filters;
 using Tochka.JsonRpc.Server.Services;
+using Tochka.JsonRpc.Server.Settings;
 
 namespace Tochka.JsonRpc.Server.Tests.Filters;
 
@@ -21,14 +24,18 @@ namespace Tochka.JsonRpc.Server.Tests.Filters;
 internal class JsonRpcExceptionFilterTests
 {
     private Mock<IJsonRpcErrorFactory> errorFactoryMock;
+    private JsonRpcServerOptions options;
+    private Mock<ILogger<JsonRpcExceptionFilter>> logMock;
     private JsonRpcExceptionFilter exceptionFilter;
 
     [SetUp]
     public void Setup()
     {
         errorFactoryMock = new Mock<IJsonRpcErrorFactory>();
+        logMock = new Mock<ILogger<JsonRpcExceptionFilter>>();
+        options = new JsonRpcServerOptions();
 
-        exceptionFilter = new JsonRpcExceptionFilter(errorFactoryMock.Object);
+        exceptionFilter = new JsonRpcExceptionFilter(errorFactoryMock.Object, Options.Create(options), logMock.Object);
     }
 
     [Test]
@@ -68,5 +75,43 @@ internal class JsonRpcExceptionFilterTests
         var expected = new ObjectResult(error);
         context.Result.Should().BeEquivalentTo(expected);
         errorFactoryMock.Verify();
+    }
+
+    [Test]
+    public void OnException_LogExceptionsTrue_LogError()
+    {
+        var exception = new ArgumentException();
+        var httpContext = new DefaultHttpContext();
+        httpContext.Features.Set<IJsonRpcFeature>(new JsonRpcFeature { Call = Mock.Of<ICall>() });
+        var actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor(), new ModelStateDictionary());
+        var context = new ExceptionContext(actionContext, new List<IFilterMetadata>())
+        {
+            Exception = exception
+        };
+        options.LogExceptions = true;
+        logMock.Setup(l => l.Log(LogLevel.Error, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), exception, It.IsAny<Func<It.IsAnyType, Exception?, string>>()))
+            .Verifiable();
+
+        exceptionFilter.OnException(context);
+
+        logMock.Verify();
+    }
+
+    [Test]
+    public void OnException_LogExceptionsFalse_DontLogAnything()
+    {
+        var exception = new ArgumentException();
+        var httpContext = new DefaultHttpContext();
+        httpContext.Features.Set<IJsonRpcFeature>(new JsonRpcFeature { Call = Mock.Of<ICall>() });
+        var actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor(), new ModelStateDictionary());
+        var context = new ExceptionContext(actionContext, new List<IFilterMetadata>())
+        {
+            Exception = exception
+        };
+        options.LogExceptions = false;
+
+        exceptionFilter.OnException(context);
+
+        logMock.VerifyNoOtherCalls();
     }
 }
