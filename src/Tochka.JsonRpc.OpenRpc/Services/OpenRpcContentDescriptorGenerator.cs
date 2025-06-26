577fec2e53f8f042eabb8434dee321660421a1fd
Sergey Bessonov <bessonov@tochka.com>
2025-06-26 18:49:43 +0500

    fix(autodoc): models from params also support Json attributes and respect nullablity

--- src/Tochka.JsonRpc.OpenRpc/Services/OpenRpcContentDescriptorGenerator.cs
+++ src/Tochka.JsonRpc.OpenRpc/Services/OpenRpcContentDescriptorGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Text.Json;
 using Namotion.Reflection;
 using Tochka.JsonRpc.Common;
@@ -24,7 +25,7 @@ public class OpenRpcContentDescriptorGenerator : IOpenRpcContentDescriptorGenera
         const bool required = false; // this method only used for collection items, collection can be empty => it's always optional
         var deprecated = type.GetAttribute<ObsoleteAttribute>(true) != null;
 
-        return Generate(type, methodName, jsonSerializerOptions, name, summary, description, required, deprecated);
+        return Generate(type, null, methodName, jsonSerializerOptions, name, summary, description, required, deprecated);
     }
 
     /// <inheritdoc />
@@ -37,25 +38,25 @@ public class OpenRpcContentDescriptorGenerator : IOpenRpcContentDescriptorGenera
         var required = !parameterMetadata.IsOptional;
         var deprecated = propertyInfo.GetAttribute<ObsoleteAttribute>(true) != null;
 
-        return Generate(type, methodName, jsonSerializerOptions, name, summary, description, required, deprecated);
+        return Generate(type, null, methodName, jsonSerializerOptions, name, summary, description, required, deprecated);
     }
 
     /// <inheritdoc />
     public OpenRpcContentDescriptor GenerateForProperty(ContextualPropertyInfo propertyInfo, string methodName, JsonSerializerOptions jsonSerializerOptions)
     {
         var type = propertyInfo.PropertyType;
-        var name = jsonSerializerOptions.ConvertName(propertyInfo.Name);
+        var name = OpenRpcSchemaGenerator.GetJsonPropertyName(propertyInfo.PropertyInfo, jsonSerializerOptions);
         var summary = propertyInfo.GetXmlDocsSummary();
         var description = propertyInfo.GetXmlDocsRemarks();
-        var required = propertyInfo.GetAttribute<RequiredAttribute>(true) != null;
+        var required = propertyInfo.GetAttribute<RequiredAttribute>(true) != null || OpenRpcSchemaGenerator.IsNotNullNullabilityContext(propertyInfo.PropertyInfo);
         var deprecated = propertyInfo.GetAttribute<ObsoleteAttribute>(true) != null;
 
-        return Generate(type, methodName, jsonSerializerOptions, name, summary, description, required, deprecated);
+        return Generate(type, propertyInfo.PropertyInfo, methodName, jsonSerializerOptions, name, summary, description, required, deprecated);
     }
 
-    private OpenRpcContentDescriptor Generate(ContextualType type, string methodName, JsonSerializerOptions jsonSerializerOptions, string name, string summary, string description, bool required, bool deprecated)
+    private OpenRpcContentDescriptor Generate(ContextualType type, PropertyInfo? property, string methodName, JsonSerializerOptions jsonSerializerOptions, string name, string summary, string description, bool required, bool deprecated)
     {
-        var schema = schemaGenerator.CreateOrRef(type, methodName, jsonSerializerOptions);
+        var schema = schemaGenerator.CreateOrRef(type, property, methodName, jsonSerializerOptions);
         return new OpenRpcContentDescriptor(name, schema)
         {
             Summary = summary,
